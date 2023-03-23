361f25a8243eaa5611247a621bca6abbdbee6588
JustinLung <justinlung77@gmail.com>
2023-03-23 17:30:03 +0100

    site header made

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	export let navTitle: string = '';
+	export let siteTitle: string = "Werkvormen & Thema's";
+</script>
+
+<header>
+	<div>
+		<img src="/assets/images/logo.svg" alt="Hogeschool van Amsterdam" />
+		<nav>Hva / Informatie / Werkvormen & thema's {navTitle}</nav>
+		<h1>{siteTitle}</h1>
+		<p>
+			Hier vind je alle werkvormen en thema’s die je kunt bekijken en materiaal van kan downloaden.
+		</p>
+	</div>
+	<img src="/assets/images/hva-triangle.svg" alt="HvA Triangle" class="graphic" />
+</header>
+
+<style>
+	img:first-child {
+		max-width: 10rem;
+	}
+
+	div {
+		padding: 2rem;
+	}
+
+	nav {
+		padding: 1rem 0;
+	}
+
+	h1 {
+		font-size: 4rem;
+		max-width: 45rem;
+		line-height: 1.3em;
+		padding: 0;
+		margin: 0;
+	}
+
+	p {
+		max-width: 30rem;
+	}
+
+	.graphic {
+		position: absolute;
+		top: 0rem;
+		z-index: -1;
+		max-width: 70%;
+	}
+
+	@media (max-width: 55rem) {
+		h1 {
+			font-size: 2rem;
+		}
+	}
+</style>
