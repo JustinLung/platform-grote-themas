88554f4c43a1615f1847c63ca115e1b62ed45011
JustinLung <justinlung77@gmail.com>
2023-04-12 19:02:17 +0200

    detail page made

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -6,3 +6,15 @@
 <video controls bind:this={video}>
 	<source src="/video/example.mp4" />
 </video>
+
+<style>
+	video {
+		width: 60%;
+	}
+
+	@media(max-width: 55rem) {
+		video {
+			width: 100%;
+		}
+	}
+</style>
