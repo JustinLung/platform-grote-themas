e60820121a4fdebfb67a9b6ab7bfff311de14fcc
JustinLung <justinlung77@gmail.com>
2023-06-01 09:52:42 +0200

    added hover animation to tag

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -4,12 +4,17 @@
 	export let color: string;
 </script>
 
-<a href={link} style={`border-color: ${color}`}><span>{title}</span></a>
+<a href={link} style={`border-color: ${color}; --hover-color: ${color}`}><span>{title}</span></a>
 
 <style>
 	a {
 		border: 2px solid;
 		font-weight: 700;
 		padding: 0.25rem 1rem;
+		transition: 100ms background-color ease-out;
+	}
+
+	:hover {
+		background-color: var(--hover-color);
 	}
 </style>
